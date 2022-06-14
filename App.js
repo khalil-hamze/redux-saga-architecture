3f1efe15bf274334ed2019620b16168ef87305c7
ba-solution <khalil.hamze@basolutions-lb.com>
2022-06-14 20:28:43 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+
+import MyStack from './src/Navigatior/AppNavigator';
+
+const App = () => {
+  return (
+    <MyStack />
+  );
+};
+
+export default App;
